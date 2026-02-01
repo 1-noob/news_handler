588daccbaf8c60a0910dc344865ef4badc4bffce
1-noob <the.noob.master.286@gmail.com>
2026-02-01 22:49:13 +0530

    created two file system

--- subroutines/rss_feed_monitor.py
+++ subroutines/rss_feed_monitor.py
@@ -15,25 +15,27 @@ class FeedWatcher:
         self.webpage = CONFIG.news_site
         self.articles = {}
         self.save_location = Path(CONFIG.CACHE_FILE)
+        self.review_location = Path(CONFIG.REVIEW_FILE)
         self.classifier = ArticleClassifier(CONFIG.SKIP_RULE, CONFIG.CLASSIFICATION_RULES)
 
         # Ensuring that the cache file exists
         self.save_location.parent.mkdir(parents=True, exist_ok=True)
+        self.review_location.parent.mkdir(parents=True, exist_ok=True)
      
-    def _read_json(self) -> dict:
+    def _read_json(self,path) -> dict:
         """Read existing JSON or returns epmty dict"""
-        if not self.save_location.exists():
+        if not path.exists():
             return {}
 
         try:
-            with open(self.save_location, 'r', encoding='utf-8') as f:
+            with open(path, 'r', encoding='utf-8') as f:
                 return json.load(f)
         except json.JSONDecodeError:
             # Corrupted JSON file
             return {}
     
-    def _write_json(self, data: dict):
-        with open(self.save_location, 'w', encoding='utf-8') as f:
+    def _write_json(self, data: dict, path: Path):
+        with open(path, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
 
 
@@ -49,29 +51,31 @@ class FeedWatcher:
 
         news_feed = feedparser.parse(self.webpage)
 
-        data = self._read_json()
-
+        classified_data = self._read_json(self.save_location)
+        review_data = self._read_json(self.review_location)
+        
         # working with news titles.
         for news in news_feed.entries:
             # Fetching the raw title 
             raw_title = news.title
             url = news.link
 
-            # Sending it to classify and clean
+            # If URL already exists in classified_data or review_data, skip it
             result = self.classifier.classify(raw_title)
-
-            # Skipping SKIPPED articles
-            if result.status == ClassificationStatus.SKIPPED:
-                continue
             
-            data[url]={
+            record = {
                 "title": result.title,
                 "category": result.category,
                 "status": result.status.name
             }
 
-        self._write_json(data)
+            if result.status == ClassificationStatus.CLASSIFIED and result.category is not None:
+                classified_data[url] = record
+            else:
+                review_data[url] = record
 
+        self._write_json(classified_data, self.save_location)
+        self._write_json(review_data, self.review_location)
             
 
 
