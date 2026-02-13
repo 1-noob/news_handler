3ead6993fb7b46f6fda77d251181bafaccd45d48
1-noob <the.noob.master.286@gmail.com>
2026-02-13 23:09:56 +0530

    added route for synchronisation

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter, HTTPException
 from subroutines.rss_feed_monitor import FeedWatcher
+from subroutines.article_synchronisation import ArticleSyncService
+from pathlib import Path
+
+import config as CONFIG
 
 router = APIRouter(prefix="/api")
 
@@ -16,5 +20,27 @@ def scan_rss():
             "stats": stats
         }
 
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.post("/proceed")
+async def proceed_sync():
+    """
+    Triggers the synchronization of articles from the cache.json file to MongoDB.
+    """
+    try:
+        service = ArticleSyncService(json_path=Path(CONFIG.CACHE_FILE))
+
+        stats = await service.sync_all()
+        
+        return {
+            "status": "success",
+            "message": "Articles synchronized successfully.",
+            "stats": stats
+        }
+    
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
