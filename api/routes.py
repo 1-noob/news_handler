7c16a52aad922b8a364574fdea2c220eb3c49ace
1-noob <the.noob.master.286@gmail.com>
2026-02-24 23:58:58 +0530

    wrote route for review_processor

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 from subroutines.rss_feed_monitor import FeedWatcher
 from subroutines.article_synchronisation import ArticleSyncService
+from subroutines.review_processor import Reviewer
 from pathlib import Path
 
 import config as CONFIG
@@ -54,3 +55,37 @@ def test_endpoint():
         "status": "ok",
         "message": "Test endpoint is working!"
     }
+
+@router.post("/review")
+def review_article(
+    action: str = Query(..., description="Action to perform on the article "),
+    url: str = Query(..., description="URL of the article to review"),
+    category: str = Query(None, description="Category for insert_prebuilt")
+):
+    """
+    Handles review actions:
+        - get_next
+        - discard
+        - insert_prebuilt
+        - insert_special
+    """
+    try:
+        review_processor = Reviewer()
+        result = review_processor.process_review_api(
+            action=action,
+            url=url,
+            category=category
+        )
+        if not result["success"]:
+            raise HTTPException(status_code=400, detail=result["message"])
+        
+        return {
+            "status": "success",
+            "message": "Review action processed successfully.",
+            "data": result["data"],
+            "state": result["status"]
+        }
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
